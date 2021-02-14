2f360832cdf857a198ac8557f9f4a5b5b2766724
YUHWAN_KIM <yuhwan.kr.kim@gmail.com>
2021-02-14 12:11:27 +0900

    multiple source code test

---
+++ Backjoon/Java/_2438.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class _2438 {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+}
