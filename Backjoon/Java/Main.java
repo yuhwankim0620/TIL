30b8f4ec3477a549d3dbddddbcae2e6ccceba535
YUHWAN_KIM <yuhwan.kr.kim@gmail.com>
2021-02-15 22:37:07 +0900

    Backjoon_2442 solve code

--- Backjoon/Java/Main.java
+++ Backjoon/Java/Main.java
@@ -1,14 +1,22 @@
-import java.util.Scanner;
-
-public class Main  {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j =1; j <=i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-}
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        //입력
+//        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+//
+//        //실행
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = n - i; j > 0; j--) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 1; k <2*i; k ++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//
+//        }
+//
+//    }
+//}
---
+++ Backjoon/Java/_2442.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //입력
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        //실행
+        for (int i = 1; i <= n; i++) {
+            for (int j = n - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <2*i; k ++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
