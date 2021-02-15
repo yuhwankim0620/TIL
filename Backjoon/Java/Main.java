5c7faadcadade15a0c985e015be80c6562efaeb9
YUHWAN_KIM <yuhwan.kr.kim@gmail.com>
2021-02-16 00:27:07 +0900

    Backjoon_2443 solve code

--- Backjoon/Java/Main.java
+++ Backjoon/Java/Main.java
@@ -1,22 +1,19 @@
-//import java.util.Scanner;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        //입력
-//        Scanner scanner = new Scanner(System.in);
-//        int n = scanner.nextInt();
-//
-//        //실행
-//        for (int i = 1; i <= n; i++) {
-//            for (int j = n - i; j > 0; j--) {
-//                System.out.print(" ");
-//            }
-//            for (int k = 1; k <2*i; k ++) {
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//
-//        }
-//
-//    }
-//}
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for(int i=1; i<=n; i++){
+            for(int k=0; k<i-1; k++){
+                System.out.print(" ");
+            }
+            for(int j= 2*n-1; j>2*(i-1); j--){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
--- Backjoon/Java/_2443.java
+++ Backjoon/Java/_2443.java
@@ -1,22 +1,19 @@
 //import java.util.Scanner;
 //
-//public class Main {
-//    public static void main(String[] args) {
-//        //입력
+//public class _2443 {
+//    public static void main(String[] args){
 //        Scanner scanner = new Scanner(System.in);
 //        int n = scanner.nextInt();
 //
-//        //실행
-//        for (int i = 1; i <= n; i++) {
-//            for (int j = n - i; j > 0; j--) {
+//        for(int i=1; i<=n; i++){
+//            for(int k=0; k<i-1; k++){
 //                System.out.print(" ");
 //            }
-//            for (int k = 1; k <2*i; k ++) {
+//            for(int j= 2*n-1; j>2*(i-1); j--){
 //                System.out.print("*");
 //            }
 //            System.out.println();
 //
 //        }
-//
 //    }
 //}
